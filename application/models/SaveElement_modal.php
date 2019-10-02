7f8a6b0d8361a3d392c8bde52fa6311938b42aa6
narinderahuja79 <narinderahuja79@gmail.com>
2019-10-02 17:17:51 +0530

    Update Files
    Update Files

--- application/models/SaveElement_modal.php
+++ application/models/SaveElement_modal.php
@@ -4,6 +4,6 @@ class SaveElement_modal extends CI_Model {
 	/* Save Element */
 	public function Insert_Element($Insert_Array){
 		$result = $this->db->insert("`elements`",$Insert_Array);
-		return $result;
+		return $this->db->insert_id();
 	}
 }
