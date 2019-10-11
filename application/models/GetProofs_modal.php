b58ead05ad1eee6cc7eb0bf4ea58e88b092700e7
mac1 <miralgaywala88@gmail.com>
2019-10-11 17:54:59 +0530

    proof and performance API, new table added
    We have added below API(s):
    - GetProofs
    - SavePerformanceElement
    - SaveProofApi
    
    We have added below modals:
    - GetProofs_modal
    - Saveperformance_modal
    - SaveProofApi_modal
    
    We have added one new table in database
    - sub_elements

---
+++ application/models/GetProofs_modal.php
@@ -0,0 +1,14 @@
+<?php 
+class GetProofs_modal extends CI_Model {
+	
+	public function Get_Proofs(){
+
+		$this->db->select("*");
+		$this->db->from("`proofs`");
+		$this->db->join('performance_elements', 'proofs.element = performance_elements.sequence','left');
+		$this->db->join('sub_elements', 'proofs.type = sub_elements.sub_elements_id','left');
+		$this->db->order_by("`proofs.id`", "asc");
+		$query_result = $this->db->get();
+		return $query_result->result();
+	}
+}
