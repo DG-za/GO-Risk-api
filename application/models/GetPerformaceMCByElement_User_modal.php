493666a706d8c4dfda64023bcf57d163b328ac85
narinderahuja79 <narinderahuja79@gmail.com>
2019-10-14 18:32:20 +0530

    New API added-GetPerformanceMCByElement_User

---
+++ application/models/GetPerformaceMCByElement_User_modal.php
@@ -0,0 +1,18 @@
+<?php 
+class GetPerformaceMCByElement_User_modal extends CI_Model {
+	
+	/* Get All Answer MC By Element_ID And User_ID */
+	public function Get_Performace_Answer_MC_by_Element_ID_and_User_ID($Element_ID,$user_id){
+		$where_Array = array(
+			"`element`" => $Element_ID,
+			"`user`" => $user_id,
+		);
+		
+		$this->db->select("`*");
+		$this->db->from("`performance_mc`");
+		$this->db->where($where_Array);
+		$this->db->order_by("`answer`", "asc");
+		$query_result = $this->db->get();
+		return $query_result->result();
+	}
+}
