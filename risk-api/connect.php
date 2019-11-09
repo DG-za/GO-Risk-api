8c0737ce408c4bf7699cdf103c89f9670c16f6a1
lalit <lalit8347@gmail.com>
2019-11-09 13:21:33 +0530

    CORS issues solved

--- risk-api/connect.php
+++ risk-api/connect.php
@@ -1,4 +1,6 @@
-<?php
+<?php header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With, Authorization");
 
 // db credentials
 define('DB_HOST', 'localhost');
