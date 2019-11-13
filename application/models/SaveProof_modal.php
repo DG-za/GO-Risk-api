2ab0ca396ade8384e1f4082e0d1a17101dfea385
narinderahuja79 <narinderahuja79@gmail.com>
2019-11-13 13:23:15 +0530

    Update SaveProof_modal.php

--- application/models/SaveProof_modal.php
+++ application/models/SaveProof_modal.php
@@ -3,7 +3,13 @@ class SaveProof_modal extends CI_Model {
 	
 	/* Comment */
 	public function Insert_Answer_Proof($Insert_Array){
-		$result = $this->db->insert("`answer_proof`",$Insert_Array);
-		return $result;
+			$this->db->select("*");
+			$this->db->from("`answer_proof`");
+			$this->db->where($Insert_Array);
+			$results=$this->db->get();
+			if($results->num_rows() == 0){
+				$result = $this->db->insert("`answer_proof`",$Insert_Array);
+				return $result;
+			}
 	}
 }
