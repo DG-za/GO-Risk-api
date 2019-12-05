5f6d73a92d17d3d6c3d1954109b7c1cea1ed5d66
lalit <lalit8347@gmail.com>
2019-12-05 12:11:57 +0530

    Rename file name

---
+++ application/models/risk/GetIncidents_modal.php
@@ -0,0 +1,17 @@
+<?php 
+class GetIncidents_modal extends CI_Model {
+	
+	/* Get Incedents */
+	public function get_Incedents($risk = Null){
+		if($risk != Null){
+			$whereArr=array(
+				"risk" => $risk
+			);
+			$this->db->where($whereArr);
+		}
+		$this->db->select('*');
+		$this->db->from('`incidents`');
+		$query_result = $this->db->get();
+		return $query_result->result();
+	}
+}
