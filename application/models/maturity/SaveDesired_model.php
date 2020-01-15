dd9a2cb09050f5b10ffece38ef9c353db5d1aa18
narinderahuja79 <narinderahuja79@gmail.com>
2020-01-15 20:33:22 +0530

    Update SaveDesired_model.php

--- application/models/maturity/SaveDesired_model.php
+++ application/models/maturity/SaveDesired_model.php
@@ -2,7 +2,8 @@
 class SaveDesired_model extends CI_Model {
 	
 	/* Insert Answer Disired */
-	public function Insert_Desired($Insert_Desired_Array){
+	public function Insert_Desired($Insert_Desired_Array,$whereArr){
+		$this->db->delete("`mat_answer_desired`",$whereArr);
 		$result = $this->db->insert('mat_answer_desired',$Insert_Desired_Array);
 		return $result;
 	}
