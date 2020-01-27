2daa93fd497c0aa1449025cf56279ab087c0a575
narinderahuja79 <narinderahuja79@gmail.com>
2020-01-27 13:20:16 +0530

    Update GetAllSessions_model.php

--- application/models/maturity/GetAllSessions_model.php
+++ application/models/maturity/GetAllSessions_model.php
@@ -2,10 +2,7 @@
 class GetAllSessions_model extends CI_Model {
 	
 	/* getAllSessions */
-	public function getAllSessions_function($user_id,$company_id=Null){
-		if($company_id!=Null){
-			$this->db->where("company_id",$company_id);
-		}
+	public function getAllSessions_function($user_id){
 		$this->db->select("mat_session.*,com_company.name as company_name");
 		$this->db->from("`mat_session`");
 		$this->db->join('com_company', 'mat_session.company_id = com_company.id','left');
